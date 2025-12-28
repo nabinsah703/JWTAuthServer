213f9a4859ffe6fc402ee568a03739736e299baf
Navin Sah <nabinsah703@gmail.com>
2025-12-28 19:17:55 +0545

    create an Dtos class that require to send only required data.

---
+++ DTOs/ProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace JWTAuthServer.DTOs
+{
+    public class ProfileDTO
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
