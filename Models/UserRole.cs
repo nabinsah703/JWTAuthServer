57cc2bb34d5f3935d04f9073a12c2d4db38e34cf
Navin Sah <nabinsah703@gmail.com>
2025-12-28 19:14:29 +0545

    create an all model class which one is required for user,user role and singing key

---
+++ Models/UserRole.cs
@@ -0,0 +1,17 @@
+﻿namespace JWTAuthServer.Models
+{
+    public class UserRole
+    {
+        // Foreign key referencing User.
+        public int UserId { get; set; }
+
+        // Navigation property to User.
+        public User User { get; set; }
+
+        // Foreign key referencing Role.
+        public int RoleId { get; set; }
+
+        // Navigation property to Role.
+        public Role Role { get; set; }
+    }
+}
